7fda4cda76d9599f3c8dcf131d20843484dd14e3
TR233 <TR233@Users-MacBook-Pro.local>
2023-01-07 16:18:45 +0900

    add fadeup.css

---
+++ fadeup.js
@@ -0,0 +1,43 @@
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top+50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+//関数でまとめることでこの後に違う動きを追加することが出来ます
+$('.fadeDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
+     var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeDown');
+// 画面内に入ったらfadeDownというクラス名を追記
+     }else{
+     $(this).removeClass('fadeDown');
+// 画面外に出たらfadeDownというクラス名を外す
+     }
+     });
+  
+  
+//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+  
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
