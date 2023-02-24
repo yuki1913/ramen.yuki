f70c4a5dda5305332be8362ad4b63eebd932bbaf
TR233 <TR233@Users-MacBook-Pro.local>
2023-02-24 23:52:57 +0900

    add ramen.js

--- fadeup.js
+++ fadeup.js
@@ -15,7 +15,7 @@ function fadeAnime(){
     });
 
   $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var elemPos = $(this).offset().top+20;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
